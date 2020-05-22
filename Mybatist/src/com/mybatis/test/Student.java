d3f1aa64fb6ab78f82cabb5fe57b3cb8171e860f
qiuxiuchang <1106663896@qq.com>
2020-05-22 22:53:00 +0800

    洗

--- Mybatist/src/com/mybatis/test/Student.java
+++ Mybatist/src/com/mybatis/test/Student.java
@@ -25,7 +25,7 @@ public class Student {
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {//set方法了
+	public void setName(String name) {
 		this.name = name;
 	}
 	public int getAge() {
